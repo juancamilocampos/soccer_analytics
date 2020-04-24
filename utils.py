047a82a130d4c9a0d5851ac998a7efae3eee05b9
Juan Camilo Campos Bustos <juan.campos@geniussportsmedia.com>
2020-04-24 09:07:55 -0500

    Updated Voronoi Diagrams

--- utils.py
+++ utils.py
@@ -6,31 +6,52 @@ from scipy.spatial import Voronoi, voronoi_plot_2d
 
 plt.ioff()
 
-def create_football_field(linenumbers=True,
-                          endzones=True,
-                          figsize=(12*2, 6.33*2)):
+def create_football_field(figsize=(12*2, 6.33*2), goals=True):
     """
     Function that plots the football field for viewing plays.
-    Allows for showing or hiding endzones.
     """
 
-    rect = patches.Rectangle((-52.5, -35), 105, 70, linewidth=3,capstyle='round',
-                             edgecolor='w', facecolor='darkgreen', alpha=0.5)
+    #pitch outline & centre line
+    pitch = patches.Rectangle((-52.5, -35), 105, 70, linewidth=2,capstyle='round',
+                             edgecolor='w', facecolor='darkgreen')
 
     fig, ax = plt.subplots(1, figsize=figsize)
     fig.patch.set_facecolor('green')
     fig.patch.set_alpha(0.7)
-    ax.add_patch(rect)
-
-    #borders
-    plt.plot([-52.5, -52.5, 52.5,52.5, -52.5], [-35, 35, 35, -35, -35], c='w', linewidth=4)
 
     ## goals
-    plt.plot([-52.5, -55, -55, -52.5], [-5, -5, 5, 5], c='w', linewidth=4)
-    plt.plot([52.5, 55, 55, 52.5], [-5, -5, 5, 5], c='w', linewidth=4)
+    if goals:
+        plt.plot([-52.5, -55, -55, -52.5], [-5, -5, 5, 5], c='w', linewidth=2)
+        plt.plot([52.5, 55, 55, 52.5], [-5, -5, 5, 5], c='w', linewidth=2)
 
     ## middle line
-    plt.plot([0,0],[-35,35],c='w', linewidth=3, alpha=0.25)
+    midline = patches.ConnectionPatch([0,-35], [0,35], "data", "data", color='white')
+
+    #center circle
+    centreCircle = plt.Circle((0,0), 10, color="white", fill = False, linewidth=2)
+    centreSpot = plt.Circle((0,0), 0.3, color="white", linewidth=2)
+
+    #left, right penalty area
+    leftPenalty = patches.Rectangle([-52.5,-15], width=14.5, height=30, fill = False,
+                                    color='white', linewidth=2)
+    rightPenalty = patches.Rectangle([38.0,-15], width=14.5, height=30, fill = False,
+                                     color='white', linewidth=2)
+
+    #left, right 6-yard box
+    leftSixYard = patches.Rectangle([-52.5,-8], width=4.5, height=16, fill=False,
+                                    color='white', linewidth=2)
+    rightSixYard = patches.Rectangle([48,-8], width=4.5, height=16, fill=False,
+                                     color='white', linewidth=2)
+
+    #penalty spots
+    leftPenSpot = plt.Circle((-43.5,0),0.3, color="white", linewidth=2)
+    rightPenSpot = plt.Circle((43.5,0),0.3, color="white", linewidth=2)
+
+    element = [pitch, midline, centreCircle, centreSpot, leftPenalty, rightPenalty, leftSixYard,
+               rightSixYard, rightPenSpot, leftPenSpot]
+
+    for i in element:
+        ax.add_patch(i)
 
     plt.xlim(-56, 56)
     plt.ylim(-37, 37)
@@ -43,14 +64,16 @@ def show_players_and_ball(row):
     ball = row[-2:]
     defense, offense = [x.reshape(-1,2) for x in row[:-2].reshape(-1,11*2)]
 
-    ax.scatter(offense[:,0],offense[:,1], c='b', s=150)
-    ax.scatter(defense[:,0],defense[:,1], c='r', s=150)
-    ax.scatter(ball[0], ball[1], c='yellow', s=50)
+    ax.scatter(offense[:,0],offense[:,1], c='b', s=150, zorder=2)
+    ax.scatter(defense[:,0],defense[:,1], c='r', s=150, zorder=2)
+    ax.scatter(ball[0], ball[1], c='yellow', s=50, zorder=2)
     return fig, ax
 
 def show_voronoi(row):
     xy = row[:-2].reshape(-1,2)
     n_points = xy.shape[0]
+
+    #To deal with unbounded Voronoi regions, I decided to mirror points across field boundaries.
     xy1 = xy.copy()
     xy1[:,1] = -35 + (-35 - xy[:,1])
     xy2 = xy.copy()
@@ -61,6 +84,7 @@ def show_voronoi(row):
     xy4[:,0] = 52.5 + (52.5 - xy[:,0])
     xy = np.concatenate((xy, xy1, xy2, xy3, xy4), axis=0)
     vor = Voronoi(xy)
+
     fig, ax = show_players_and_ball(row)
     #voronoi_plot_2d(vor, ax=ax, show_points=False, show_vertices=False)
     for r in range(n_points):
@@ -68,8 +92,10 @@ def show_voronoi(row):
         if not -1 in region:
             polygon = [vor.vertices[i] for i in region]
             if r>10:
+                #Offense players
                 plt.plot(*zip(*polygon), c='black', alpha=0.25, linewidth=3)
             else:
+                #Defense players
                 plt.fill(*zip(*polygon), c='r', alpha=0.25, edgecolor="black", linewidth=0.0)
                 plt.plot(*zip(*polygon), c='black', alpha=0.25, linewidth=3)
 
